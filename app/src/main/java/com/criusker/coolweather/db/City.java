f64b05bf3f4b75edfaabf40738a6a29742c0088a
crius_keer <973968629@qq.com>
2018-12-25 23:53:52 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/criusker/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.criusker.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Create by 李菀直 on 2018/12/25.
+ * 实体类：城市
+ */
+public class City extends LitePalSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/criusker/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.criusker.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Create by 李菀直 on 2018/12/25.
+ * 实体类：省
+ */
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
